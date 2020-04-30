6e9e73ae91c40a50b7d9ebd376032f790d338af8
biancad2 <36548859+biancad2@users.noreply.github.com>
2020-04-30 12:54:53 -0300

    Adiciona gerador de token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
 
 const User = require('../models/User');
 
 const router = express.Router();
 
+
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    });
+}
 router.post('/register', async (req, res) => {
     const { email } = req.body;
     try {
@@ -13,7 +22,10 @@ router.post('/register', async (req, res) => {
         const user = await User.create(req.body);
 
         user.password = undefined;
-        return res.send({ user });
+        return res.send({
+            user,
+            token: generateToken({ id: user.id }),
+        });
 
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
@@ -31,7 +43,13 @@ router.post('/authenticate', async (req, res) => {
     if (!await bcrypt.compare(password, user.password))
         return res.status(400).send({ error: "invalid password" });
 
-    res.send({user});
+    user.password = undefined;
+
+
+    res.send({
+        user,
+        token: generateToken({ id: user.id })
+    });
 });
 
 module.exports = app => app.use('/auth', router);
